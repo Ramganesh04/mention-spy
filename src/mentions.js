92c5bcf46a3f77fde2e51a24aa010ae1cfe41f8e
Ramganesh04 <120028035+Ramganesh04@users.noreply.github.com>
2024-11-13 10:49:33 +0530

    aws

--- src/mentions.js
+++ src/mentions.js
@@ -1,30 +1,37 @@
-export async function checkMentions(msg) {
-  let count = 0;
-  if (msg.mentions.users.size >= 2) {
-    const mentionedUsersArray = Array.from(msg.mentions.users.values());
-    const user1 = mentionedUsersArray[0];
-    const user2 = mentionedUsersArray[1];
-    let messages = await msg.channel.messages.fetch({
-      limit: 100,
-      cache: false,
-    });
-    try {
-      messages
-        .forEach((message) => {
-          if (
-            message.mentions.users.size > 0 &&
-            message.author.id == user1.id
-          ) {
-            message.mentions.users.forEach((user) => {
-              if (user.id == user2.id) {
-                count = count + 1;
-              }
-            });
-          }
-        })
-    } catch (err) {
-      console.log(err);
-    }
-    return { mentionedUsersArray, count };
-  }
+export async function checkMentions(msg, user1, user2, messageLimit) {
+	let count = 0;
+	if (msg.mentions.users.size >= 2) {
+		let last_message = msg;
+		while (true) {
+			let t0 = performance.now();
+			const messages = await msg.channel.messages.fetch({
+				before: last_message.id,
+				limit: messageLimit,
+				cache: false,
+			});
+			let t1 = performance.now();
+			console.log(t1 - t0);
+			let r_count = 0;
+			try {
+				messages.forEach((message) => {
+					r_count++;
+					last_message = message;
+					if (
+						message.mentions.users.size > 0 &&
+						message.author.id == user1.id
+					) {
+						message.mentions.users.forEach((user) => {
+							if (user.id == user2.id) {
+								count++;
+							}
+						});
+					}
+				});
+			} catch (err) {
+				console.log(err);
+			}
+			if (r_count < messageLimit) break;
+		}
+	}
+	return { count };
 }
